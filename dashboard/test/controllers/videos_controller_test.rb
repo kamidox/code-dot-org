c81222952cda704bf45a4a0e0f0e7ef6f487e8d4
Will Jordan <will@code.org>
2016-08-12 16:25:51 -0700

    revert minor changes

--- dashboard/test/controllers/videos_controller_test.rb
+++ dashboard/test/controllers/videos_controller_test.rb
@@ -1,15 +1,3 @@
-# == Schema Information
-#
-# Table name: videos
-#
-#  id           :integer          not null, primary key
-#  key          :string(255)
-#  youtube_code :string(255)
-#  created_at   :datetime
-#  updated_at   :datetime
-#  download     :string(255)
-#
-
 require 'test_helper'
 
 class VideosControllerTest < ActionController::TestCase
